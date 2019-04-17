16a91e8bfe8d22099ec57c3de4d7e226e05ef21f
曾zy_实验室 <904156108@qq.com>
2019-04-17 16:17:37 +0800

    4-17

--- userInfor/urls.py
+++ userInfor/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login),
-    # path("login_handle/", views.login_handle),
     path("logout/", views.logout),
-    # path("register/", views.register),
     path("register_handle/", views.register_handle),
     path("upload/", views.upload),
-    # path("upload_handle/", views.upload_handle),
     path("test/", views.test),
     path("count/",views.count),
     path("count/edit/",views.countEdit),
